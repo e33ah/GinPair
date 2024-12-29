48c53f62e7a4158eef7b50cd765975d7a889669d
ema <189465602+e33ah@users.noreply.github.com>
2024-12-29 19:35:44 +0000

    make tonic brand required

---
+++ GinPair/Migrations/20241229193215_tonicBrandRequired.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GinPair.Migrations
+{
+    /// <inheritdoc />
+    public partial class tonicBrandRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "tonic_brand",
+                schema: "gp_schema",
+                table: "tonics",
+                type: "varchar",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "tonic_brand",
+                schema: "gp_schema",
+                table: "tonics",
+                type: "varchar",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar",
+                oldMaxLength: 100);
+        }
+    }
+}
--- GinPair/Models/Tonic.cs
+++ GinPair/Models/Tonic.cs
@@ -3,7 +3,7 @@
     public class Tonic
     {
         public int TonicId { get; set; }
-        public string? TonicBrand { get; set; }
+        public string TonicBrand { get; set; }
         public string TonicFlavour { get; set; }
         public ICollection<Pairing> Pairings { get; set; }
 
