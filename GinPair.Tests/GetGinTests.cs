ae78ff3cc775616f716458da2d6f232a2e24cbcd
ema <189465602+e33ah@users.noreply.github.com>
2025-02-09 15:44:38 +0000

    add nogin present test

--- GinPair.Tests/GetGinTests.cs
+++ GinPair.Tests/GetGinTests.cs
@@ -48,4 +48,23 @@ public class GetGinTests
         var okResult = Assert.IsType<OkObjectResult>(result);
         Assert.IsType<ApiResponse>(okResult.Value);
     }
+    [Fact]
+    public async Task GetPairingById_NoGin_Returns_Bad()
+    {
+        var options = GetDbContextOptions();
+        using var mockContext = new GinPairDbContext(options);
+        mockContext.Pairings.AddRange(new List<Pairing>
+        {
+            new() {
+                PairedGin = new Gin {GinId = 3, GinName = "TestName3", Distillery = "TestDis3"},
+                PairedTonic = new Tonic {TonicId = 3, TonicBrand = "TestBrand3", TonicFlavour = "TestFl3"}
+            }
+        });
+        mockContext.SaveChanges();
+        var controller = new GinApiController(mockContext);
+
+        var result = await controller.GetPairingByGinId(4);
+        Assert.NotNull(result);
+        Assert.IsType<BadRequestResult>(result);
+    } 
 }
